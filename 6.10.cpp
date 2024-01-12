0244c33ca4fdb52e2a6bbf3dad4028e081660d2a
MOSS <15186316@qq.com>
2024-01-12 22:07:30 +0800

    1

--- 6.10.cpp
+++ 6.10.cpp
@@ -1,10 +1,47 @@
-#include <iostream>
-
-int main()
-{
-	using namespace std;
-
-
-
-	return 0;
-}
+//#include <iostream>
+//void showmenu();
+//void report();
+//void comfort();
+//int main()
+//{
+//	using namespace std;
+//	showmenu();
+//	int choice;
+//	cin >> choice;
+//	while (choice != 5)
+//	{
+//		switch (choice) 
+//		{
+	//		case 1: cout << "\a\n";
+	//			break;
+	//		case 2: report();
+	//			break;
+	//		case 3: cout << "The boss was in all day";
+	//			break;
+	//		case 4: comfort();
+	//		default: cout << " Thats not a choice.\n";
+//		}
+//		showmenu();
+//		cin >> choice;
+//	}
+//	cout << "bye!\n";
+//	return 0;
+//}
+//
+//void showmenu()
+//{
+//	std::cout << "Pls enter 1,2,3,4 or 5:\n"
+//		"1) alarm        2) report\n"
+//		"3) alibi        4) comfort\n"
+//		"5) quit\n";
+//}
+//
+//void report()
+//{
+//	std::cout << "report~\n";
+//}
+//
+//void comfort()
+//{
+//	std::cout << "comfort!\n";
+//}
--- 6.11.cpp
+++ 6.11.cpp
@@ -1,9 +1,34 @@
 //#include <iostream>
-//
+//enum { red, orange, yellow, green, blue, viiolet, indigo};
 //int main()
 //{
 //	using namespace std;
-//
-//
+//	cout << "Enter color code(0-6): ";
+//	int code;
+//	cin >> code;
+//	while (code >= red && code <= indigo)
+//	{
+//		switch (code)
+//		{
+//			case red: cout << "red!\n";
+//				break;
+//			case orange: cout << "orange!\n";
+//				break;
+//			case yellow: cout << "yellow!\n";
+//				break;
+//			case green: cout << "green!\n";
+//				break;
+//			case blue: cout << "blue!\n";
+//				break;
+//			case viiolet: cout << "viiolet!\n";
+//				break;
+//			case indigo: cout << "indigo!\n";
+//				break;
+//		}
+//		cout << "Enter color code(0-6): ";
+//		cin >> code;
+//	}
+//	cout << "Bye\n";
+//	
 //	return 0;
 //}
