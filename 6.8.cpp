a4e2eea8634e4d01f7e3e617b71bb8d38028cbd0
MOSS <15186316@qq.com>
2024-01-12 18:58:15 +0800

    new

--- 6.8.cpp
+++ 6.8.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
-
-int main()
-{
-	using namespace std;
-
-
-	return 0;
-}
+//#include <iostream>
+//#include <cctype>
+//
+//int main()
+//{
+//	using namespace std;
+//	cout << "Enter text for anaysis, and type @ to terminate input.\n";
+//	char ch;
+//	int whitespace = 0;
+//	int digits = 0;
+//	int chars = 0;
+//	int punct = 0;
+//	int others = 0;
+//
+//	cin.get(ch);
+//	while (ch != '@')
+//	{
+//		if (isalpha(ch))
+//			chars++;
+//		else if (isspace(ch))
+//			whitespace++;
+//		else if (isdigit(ch))
+//			digits++;
+//		else if (ispunct(ch))
+//			punct++;
+//		else
+//			others++;
+//		cin.get(ch);
+//	}
+//	cout << chars << "letters," << whitespace << " whitespace " << digits << " digits " << punct << " punct " << others << " others.\n";
+//	return 0;
+//}
---
+++ 6.9.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	using namespace std;
+//	int a, b;
+//	cout << "Enter tow integers:";
+//	cin >> a >> b;
+//	cout << "The larger of " << a << " and " << b;
+//	int c = a > b ? a : b;
+//	cout << " is " << c << endl;
+//
+//
+//	return 0;
+//}
